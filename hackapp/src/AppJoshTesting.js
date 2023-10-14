65fc5bcae34b14e984c688c987faa0e310a8c0f2
Josh Pearlson <Jcpearlson@gmail.com>
2023-10-14 14:54:29 -0500

    cmJosh2

---
+++ hackapp/src/AppJoshTesting.js
@@ -0,0 +1,24 @@
+// this stores josh's testing script for app.js
+import logo from './logo.svg';
+import './App.css';
+import supabase from './supabase';
+import TableData from './TableData';
+import FileUploader from './FileUploader';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h4>Supabase db Uploader</h4>
+        <FileUploader />
+
+      </header>
+        <TableData />
+    </div>
+  );
+}
+
+export default App;
+
+
