540a7e4096738fb097875c7e1493fdd933f8764f
Jasper Sands <jasper@mindtrip.ai>
2023-10-14 22:39:28 -0500

    delete unused

--- hackapp/src/App.js
+++ hackapp/src/App.js
@@ -4,8 +4,6 @@ import './App.css';
 import Header from './Header';
 import TableData from './TableData';
 import FileUploader from './FileUploader';
-import Login from './Login';
-import CreateAccount from './CreateAccount';
 import Welcome from './Welcome2';
 import { AuthProvider, useAuth } from './AuthContext';
 
@@ -17,8 +15,6 @@ function App() {
       <Router>
         <Routes>
         <Route path="/" element={<WelcomeWithoutHeader><Welcome /></WelcomeWithoutHeader>} />
-          <Route path="/login" element={<LoginWithoutHeader><Login /></LoginWithoutHeader>} />
-          <Route path="/createaccount" element={<LoginWithoutHeader><CreateAccount /></LoginWithoutHeader>} />
           <Route path="/listings" element={<DefaultLayout><FileUploader/></DefaultLayout>} />
           <Route path="/newlisting" element={<NewListing><FileUploader /></NewListing>} />
         
@@ -53,16 +49,6 @@ const NewListing = () => (
 );
 
 
-
-// Define a layout without the header for the login page
-const LoginWithoutHeader = ({ children }) => (
-  <div>
-    <Routes>
-      <Route path="/" element={children} />
-    </Routes>
-  </div>
-);
-
 const WelcomeWithoutHeader = ({ children }) => (
   <div>
     <Routes>
@@ -70,7 +56,6 @@ const WelcomeWithoutHeader = ({ children }) => (
     </Routes>
   </div>
 );
-
 
 
 export default App;
