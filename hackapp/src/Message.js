a338456ce77a8dd74373f9a9e8586788a98f9e76
Jasper Sands <jasper@mindtrip.ai>
2023-10-15 07:58:57 -0500

    messaging done

--- hackapp/src/Message.js
+++ hackapp/src/Message.js
@@ -2,38 +2,44 @@
 
 import React, { useEffect, useState } from 'react';
 import supabase from './supabase';
-import { useLocation } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import ConversationCard from './components/conversationcard';
-import ApartmentPopup from './listingPopUp';
-import "./index.css"
+import "./message.css"
 import { useAuth } from './AuthContext'; // Assuming you've created AuthContext.js
 
 function Message() {
     const [conversations, setConversations] = useState([]);
-    const [selectedConversation, setSelectedConversation] = useState(null);
+    const [selectedConversation, setSelectedConversation] = useState();
     const [selectedMessages, setSelectedMessages] = useState(null);
-    const [messages, setMessages] = useState([]);
     const [newMessage, setNewMessage] = useState('');
     const navigate = useNavigate();
     const auth = useAuth(); // Assuming AuthContext provides user information
     const { loggedIn } = useAuth();
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
-    let x = '_'
+    const [title, setTitle] = useState("place");
+
     useEffect(() => {
-        
         // Check if the user is already logged in
-        if (loggedIn == -1) {
-        // Redirect or handle as needed
-        // Example: Redirect to the home page
-        navigate('/');
+        if (loggedIn === -1) {
+            // Redirect or handle as needed
+            // Example: Redirect to the home page
+            navigate('/');
         }
     }, [auth]);
 
+    useEffect(() => {
+        // This effect runs whenever selectedConversation changes
+        if (selectedConversation) {
+            // Fetch messages and title when selectedConversation changes
+            fetchMessages();
+            fetchTitle();
+        }
+    }, [selectedConversation]);
+
     function back() {
         navigate('/listings');
-      }
+    }
 
     useEffect(() => {
         async function fetchData() {
@@ -52,9 +58,9 @@ function Message() {
                     }));
                     const uniqueConversations = {};
                     dataWithConversationId.forEach(message => {
-                    if (!uniqueConversations[message.conversationId] || message.created_at > uniqueConversations[message.conversationId].created_at) {
-                        uniqueConversations[message.conversationId] = message;
-                    }
+                        if (!uniqueConversations[message.conversationId] || message.created_at > uniqueConversations[message.conversationId].created_at) {
+                            uniqueConversations[message.conversationId] = message;
+                        }
                     });
                     const uniqueConversationsArray = Object.values(uniqueConversations);
                     setConversations(uniqueConversationsArray);
@@ -66,24 +72,36 @@ function Message() {
             }
         }
         fetchData();
-    }, []);
+    }, [loggedIn]);
 
-    if (loading) {
-        return <p>Loading...</p>;  // You can replace this with a loading spinner or any other loading indicator
-    }
+    const fetchTitle = async () => {
+        try {
+            const [sender, receiver] = selectedConversation.split('_');
+            const { data: user, error: emailError } = await supabase
+                .from('users')
+                .select('*')
+                .eq('id', sender == loggedIn ? receiver : sender)
+                .single();
 
-    
+            if (user) {
+                setTitle(`${user.firstname} ${user.lastname}`);
+            }
+        } catch (error) {
+            console.error('Error fetching user data:', error);
+        }
+    };
 
     const fetchMessages = async () => {
-        if(selectedConversation){
-            //Implement fetching messages from your database (Supabase in this example)
+        //setTimeout(250);
+        console.log(selectedConversation);
+        if (selectedConversation) {
             const [sender, receiver] = selectedConversation.split('_');
             const { data, error } = await supabase
                 .from('messages')
                 .select('*');
             const privateMes = [];
             data.forEach(message => {
-                if ((message.sender == sender && message.receiver == receiver)|| (message.sender == receiver && message.receiver == sender)) {
+                if ((message.sender == sender && message.receiver == receiver) || (message.sender == receiver && message.receiver == sender)) {
                     privateMes.push(message);
                 }
             });
@@ -92,15 +110,13 @@ function Message() {
         }
     };
 
-
     const handleSendMessage = async () => {
-        //Implement sending a new message to the selected conversation
         if (selectedConversation) {
             const [sender, receiver] = selectedConversation.split('_');
             await supabase.from('messages').insert([
                 {
                     sender: loggedIn,
-                    receiver: sender == loggedIn? receiver : sender,
+                    receiver: sender == loggedIn ? receiver : sender,
                     text: newMessage,
                 },
             ]);
@@ -111,23 +127,31 @@ function Message() {
         }
     };
 
+    
+
     return (
-        <div>
-            <button className="back" onClick={back}>
-            Back
-            </button>
-            {loading ? (
-                <p>Loading...</p>
-            ) : conversations.length === 0 ? (
-                <p>You have no messages.</p>
-            ) : (
-                <div>
+        <div className="message-container">
+            <div className="conversation-list-container">
+                <button className="back" onClick={back}>
+                    Back
+                </button>
+                {loading ? (
+                    <p>Loading...</p>
+                ) : conversations.length === 0 ? (
+                    <p>You have no messages.</p>
+                ) : (
                     <div className="conversation-list">
                         {conversations.map(conversation => (
-                            <li key={conversation.conversationId} onClick={async () => {
-                                await setSelectedConversation(conversation.conversationId);
-                                await fetchMessages();
-                            }}>
+                            <li
+                                key={conversation.conversationId}
+                                onClick={async() => {
+                                     await setSelectedConversation(
+                                        conversation.conversationId
+                                    );
+                                     
+                                    
+                                }}
+                            >
                                 <ConversationCard
                                     conversation={{
                                         text: conversation.text,
@@ -139,20 +163,35 @@ function Message() {
                             </li>
                         ))}
                     </div>
-                </div>
-            )}
+                )}
+            </div>
 
-            <div className="message-list">
+            <div className="message-list-container">
                 {selectedMessages && (
-                    <div>
-                        <h2>{selectedMessages[0].firstname + " " + selectedMessages[0].lastname}</h2>
-                        {selectedMessages.map(message => (
-                            <li key={message.id}>
+                    <div className="message-list">
+                        <h2>
+                            {title}
+                        </h2>
+                        {selectedMessages.map((message) => (
+                            <li
+                                key={message.id}
+                                className={
+                                    message.sender === loggedIn
+                                        ? 'sent-message'
+                                        : 'received-message'
+                                }
+                            >
                                 {message.text}
                             </li>
                         ))}
                         <div>
-                            <input type="text" value={newMessage} onChange={(e) => setNewMessage(e.target.value)} />
+                            <input
+                                type="text"
+                                value={newMessage}
+                                onChange={e =>
+                                    setNewMessage(e.target.value)
+                                }
+                            />
                             <button onClick={handleSendMessage}>Send</button>
                         </div>
                     </div>
